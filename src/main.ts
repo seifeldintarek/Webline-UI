df7955e43737c4450388684c28e1a37c845ec7b9
BeshoyHani <be4oy.hani@gmail.com>
2025-09-14 23:48:36 +0300

    merge main branch and fix config in main.ts file

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,7 @@
 import { bootstrapApplication } from '@angular/platform-browser';
-import { provideRouter } from '@angular/router';
 import { AppComponent } from './app/app.component';
-import { routes } from './app/app.routes';
+import { appConfig } from './app/app.config';
 
-
-bootstrapApplication(AppComponent, {
-  providers: [
-    provideRouter(routes)
-  ]
-});
+bootstrapApplication(AppComponent, appConfig).catch((err) =>
+  console.error(err),
+);
