a921295cc588817779e18a4772a63afcacbacce8
seif tarek <seiftarek211200@gmail.com>
2025-09-15 18:52:30 +0300

    login service

---
+++ src/app/friendship/friendship.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-friendship',
+  standalone: true,
+  imports: [],
+  templateUrl: './friendship.component.html',
+  styleUrl: './friendship.component.scss'
+})
+export class Friendship {
+
+}
---
+++ src/app/models/friendship-model.ts
@@ -0,0 +1,2 @@
+export interface FriendshipModel {
+}
