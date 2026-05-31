96603fcad424183eb6fa2455e0a0562ad764a090
seifeldintarek <seiftarek211200@gmail.com>
2026-05-31 05:50:44 +0300

    search screen UI updated

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -20,7 +20,7 @@ export class AppComponent implements OnInit {
   ngOnInit() {
     this.authService.validateToken().subscribe(isValid => {
       if (isValid) {
-        this.router.navigate(['/home']);
+        this.router.navigate(['/home/friends']);
       }
       this.isLoading = false;
     });
