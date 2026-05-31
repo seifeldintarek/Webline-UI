086c06b84f7030389167e3578742959645812d3c
seifeldintarek <seiftarek211200@gmail.com>
2026-05-31 18:07:19 +0300

    message service deployment

--- src/app/environments/environment.prod.ts
+++ src/app/environments/environment.prod.ts
@@ -1,7 +1,7 @@
 export const environment = {
   production: true,
-  userServiceUrl: 'https://USER_SERVICE_URL.up.railway.app/api/users',
-  messageServiceUrl: 'https://MSG_SERVICE_URL.up.railway.app/api',
+  userServiceUrl: 'https://webline-server-production.up.railway.app/api/users',
+  messageServiceUrl: 'https://webline-server.onrender.com/api'', // TODO: fill after deploying message-service
   zego: {
     appId: 1470021379,
     serverSecret: '5a8fb71ee6d7aaf30709b147b49cfb22'
